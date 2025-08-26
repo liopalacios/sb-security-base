a2da18bfc57f3677db025fc6504918003a533e9b
Leyter <leyterpalacios@gmail.com>
2025-08-25 23:13:44 -0500

    agregar get controller pingpong

--- digital/src/main/java/com/zennitech/digital/controller/UsuarioController.java
+++ digital/src/main/java/com/zennitech/digital/controller/UsuarioController.java
@@ -25,4 +25,8 @@ public class UsuarioController {
     public String ping() {
         return "pong - " + System.currentTimeMillis();
     }
+    @GetMapping("/pong")
+    public String pong() {
+        return "pong ping- " + System.currentTimeMillis();
+    }
 }
