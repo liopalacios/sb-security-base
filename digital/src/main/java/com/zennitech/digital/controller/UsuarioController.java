b122a216a0d0ae22f2ad9bde8777b82981b6fb8c
Leyter <leyterpalacios@gmail.com>
2025-09-18 16:33:56 -0500

    cambios realizados en lista de usuarios

--- digital/src/main/java/com/zennitech/digital/controller/UsuarioController.java
+++ digital/src/main/java/com/zennitech/digital/controller/UsuarioController.java
@@ -38,7 +38,8 @@ public class UsuarioController {
     public List<UsuarioModel> listarUsuarios() {
         return List.of(
                 new UsuarioModel(1L, "Ana", "ana@example.com",null,null),
-                new UsuarioModel(2L, "Pedro", "pedro@example.com",null, null)
+                new UsuarioModel(2L, "Pedro", "pedro@example.com",null, null),
+                new UsuarioModel(3L, "Pablo", "pablo@example.com",null, null)
         );
     }
 }
