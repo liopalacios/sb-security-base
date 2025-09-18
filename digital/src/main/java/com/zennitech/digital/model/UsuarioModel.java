52b61bf95c3318f40ae6f62405b5a69fdcc5e5ac
Leyter <leyterpalacios@gmail.com>
2025-09-18 16:40:22 -0500

    agregar constructor vacio

--- digital/src/main/java/com/zennitech/digital/model/UsuarioModel.java
+++ digital/src/main/java/com/zennitech/digital/model/UsuarioModel.java
@@ -2,10 +2,12 @@ package com.zennitech.digital.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioModel {
     private Long id;
     private String username;
