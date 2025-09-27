17a3044cbac6d52b1319bc69231cd39a9c6eb5da
Leyter <leyterpalacios@gmail.com>
2025-09-27 15:19:35 -0500

    actualizacion controller-10

--- digital/src/main/java/com/zennitech/digital/controller/UsuarioController.java
+++ digital/src/main/java/com/zennitech/digital/controller/UsuarioController.java
@@ -39,6 +39,7 @@ public class UsuarioController {
         return List.of(
                 new UsuarioModel(1L, "Ana", "ana@example.com",null,null),
                 new UsuarioModel(2L, "Pedro", "pedro@example.com",null, null),
+                new UsuarioModel(2L, "Juan", "juan@example.com",null, null),
                 new UsuarioModel(2L, "Ana", "ana@example.com",null, null),
                 new UsuarioModel(3L, "Pablo", "pablo@example.com",null, null)
         );
